77cae1c160770acf0e1b527d454f99f9c4b03d82
akghosh111 <ghoshkiran2017@gmail.com>
2024-07-10 15:55:14 +0530

    selecting language & clicking cookie

--- cookiedemo.py
+++ cookiedemo.py
@@ -9,4 +9,23 @@ import time
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://google.com")
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+cookie_id = "bigCookie"
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_all_elements_located((By.XPATH, "//*[contains(text(), 'English')]"
+))
+)
+
+language = driver.find_element(By.XPATH,"//*[contains(text(), 'English')]")
+language.click()
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_all_elements_located((By.ID, cookie_id))
+)
+
+cookie = driver.find_element(By.ID, cookie_id)
+cookie.click()
+
+time.sleep(10)
